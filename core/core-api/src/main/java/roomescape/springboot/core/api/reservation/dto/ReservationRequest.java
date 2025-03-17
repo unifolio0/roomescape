a8db4fbb24157d605a30f45f16ab9056cb11ea98
unifolio0 <boho82ha@gmail.com>
2025-03-17 14:28:25 +0900

    refactor: 입력값 검증

--- core/core-api/src/main/java/roomescape/springboot/core/api/reservation/dto/ReservationRequest.java
+++ core/core-api/src/main/java/roomescape/springboot/core/api/reservation/dto/ReservationRequest.java
@@ -1,6 +1,20 @@
 package roomescape.springboot.core.api.reservation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public record ReservationRequest(String name, LocalDate date, Long timeId) {
+public record ReservationRequest(
+        @NotNull
+        @NotBlank
+        String name,
+
+        @NotNull
+        @NotBlank
+        LocalDate date,
+
+        @NotNull
+        @NotBlank
+        Long timeId
+) {
 }
--- core/core-api/src/main/java/roomescape/springboot/core/api/reservation_time/dto/ReservationTimeRequest.java
+++ core/core-api/src/main/java/roomescape/springboot/core/api/reservation_time/dto/ReservationTimeRequest.java
@@ -1,6 +1,12 @@
 package roomescape.springboot.core.api.reservation_time.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
-public record ReservationTimeRequest(LocalTime startAt) {
+public record ReservationTimeRequest(
+        @NotNull
+        @NotBlank
+        LocalTime startAt
+) {
 }
